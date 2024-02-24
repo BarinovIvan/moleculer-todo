2a51d34edca6c31360f179b2e02753d084cfd83c
Иван Баринов <i.barinov@itlabrealty.ru>
2024-02-24 10:51:54 +0300

    refactored api.service.js

--- services/api.service.js
+++ services/api.service.js
@@ -5,8 +5,6 @@ const ApiGateway = require('moleculer-web');
 module.exports = {
 	name: 'api',
 	mixins: [ApiGateway],
-
-	/** @type {ApiSettingsSchema} More info about settings: https://moleculer.services/docs/0.14/moleculer-web.html */
 	settings: {
 		port: process.env.PORT || 3000,
 		ip: '0.0.0.0',
@@ -16,47 +14,10 @@ module.exports = {
 				whitelist: [
 					'**'
 				],
-				// Enable/disable parameter merging method. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Disable-merging
 				mergeParams: true,
 				authentication: true,
 				authorization: false,
-
-				// The auto-alias feature allows you to declare your route alias directly in your services.
-				// The gateway will dynamically build the full routes from service schema.
 				autoAliases: true,
-
-				aliases: {
-
-				},
-
-				/**
-				 * Before call hook. You can check the request.
-				 * @param {Context} ctx
-				 * @param {Object} route
-				 * @param {IncomingRequest} req
-				 * @param {ServerResponse} res
-				 * @param {Object} data
-				 *
-				onBeforeCall(ctx, route, req, res) {
-					// Set request headers to context meta
-					ctx.meta.userAgent = req.headers["user-agent"];
-				}, */
-
-				/**
-				 * After call hook. You can modify the data.
-				 * @param {Context} ctx
-				 * @param {Object} route
-				 * @param {IncomingRequest} req
-				 * @param {ServerResponse} res
-				 * @param {Object} data
-				onAfterCall(ctx, route, req, res, data) {
-					// Async function which return with Promise
-					return doSomething(ctx, res, data);
-				}, */
-
-				// Calling options. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Calling-options
-				callOptions: {},
-
 				bodyParsers: {
 					json: {
 						strict: false,
@@ -67,36 +28,21 @@ module.exports = {
 						limit: '1MB'
 					}
 				},
-
-				// Mapping policy setting. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Mapping-policy
-				mappingPolicy: 'all', // Available values: "all", "restrict"
-
-				// Enable/disable logging
+				mappingPolicy: 'all',
 				logging: true
-			}
+			},
 		],
-
-		// Do not log client side errors (does not log an error response when the error.code is 400<=X<500)
 		log4XXResponses: false,
-		// Logging the request parameters. Set to any log level to enable it. E.g. "info"
 		logRequestParams: null,
-		// Logging the response data. Set to any log level to enable it. E.g. "info"
 		logResponseData: null,
-
-
-		// Serve assets from "public" folder. More info: https://moleculer.services/docs/0.14/moleculer-web.html#Serve-static-files
 		assets: {
 			folder: 'public',
-
-			// Options to `server-static` module
 			options: {}
 		}
 	},
 
 	methods: {
 		async authenticate(ctx, route, req) {
-			// Read the token from header
-			const auth = req.headers['authorization'];
 			if (req.url === '/users/login') {
 				console.log('Bypassing the authentication for logging in');
 				return;
@@ -125,15 +71,5 @@ module.exports = {
 			}
 
 		},
-		async authorize(ctx, route, req) {
-			// Get the authenticated user.
-			const user = ctx.meta.user;
-
-			// It check the `auth` property in action schema.
-			if (req.$action.auth == 'required' && !user) {
-				throw new ApiGateway.Errors.UnAuthorizedError('NO_RIGHTS');
-			}
-		}
-
 	}
 };
